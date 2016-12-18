6dd5da5679e1bf18497d0855f44779a5ec7b6574
Adam VanLente <adamvanlente@Adam-VanLentes-Macbook-pro.local>
2016-12-18 14:05:24 -0500

    Add sample component, remove hello component.

---
+++ template/src/components/Sample.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="sample">
+    <h1>{{ msg }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'sample',
+  data{{#unless_eq lintConfig "airbnb"}} {{/unless_eq}}() {
+    return {
+      msg: 'Sample template'{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+    }{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+  }{{#if_eq lintConfig "airbnb"}},{{/if_eq}}
+}{{#if_eq lintConfig "airbnb"}};{{/if_eq}}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style lang="sass" scoped>
+  h1
+    font-weight: normal
+</style>
